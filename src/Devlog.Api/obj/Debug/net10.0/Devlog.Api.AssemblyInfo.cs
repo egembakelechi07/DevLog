c2cd539846c49bd1cd6fadac2f76294e3429eb4e
egembakelechi07 <egembakelechi07@gmail.com>
2026-05-17 23:04:28 +0100

    Fix Solution File

--- src/Devlog.Api/obj/Debug/net10.0/Devlog.Api.AssemblyInfo.cs
+++ src/Devlog.Api/obj/Debug/net10.0/Devlog.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Devlog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea2de13c7bb7fcdcde4713b390a05128d5f5ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Devlog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Devlog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
